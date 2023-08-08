b71f3ab5b2a8076e142fc28e9225962ced50a810
Yu-Kai Lin <scott421722@gmail.com>
2023-08-08 17:16:05 +0800

    try JavaScript

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+var myHeading = document.querySelector("h2");
+myHeading.textContent = "Hello world!";
+alert("hello!");
+
+document.querySelector("html").onclick = function () {
+    alert("Ouch! Stop poking me!");
+  };
